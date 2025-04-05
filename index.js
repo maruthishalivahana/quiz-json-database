d9997c0d075906d7ad30bdc6e73025c6a15627b5
maruthi <maruthikummari553@gmail.com>
2025-04-05 14:24:26 +0530

    changed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const router1 = jsonServer.router("cyber.json");
+const router2 = jsonServer.router("debug.json");
+const router3 = jsonServer.router("tech.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
